2d038d5956547a6acf3a1a775367d2554486c1a8
smartpeople <semeyon@speople.pro>
2017-05-31 16:32:03 +0600

    Initial commit

---
+++ lib/shortbot.ex
@@ -0,0 +1,18 @@
+defmodule Shortbot do
+  @moduledoc """
+  Documentation for Shortbot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Shortbot.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/shortbot_test.exs
@@ -0,0 +1,8 @@
+defmodule ShortbotTest do
+  use ExUnit.Case
+  doctest Shortbot
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
